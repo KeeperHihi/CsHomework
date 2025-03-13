731062a7d8a1af26f7dc554ba3c5330388c00536
KeeperHihi <rain906@126.com>
2025-03-13 15:51:05 +0800

    Update CsHomework4

--- assignment4/ClockTask/Program.cs
+++ assignment4/ClockTask/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ClockTask {
+	public class TikTokEventArgs {
+		public int time { get; set; }	
+	}
+	public delegate void TikTokHandler(object sender, TikTokEventArgs args);
+	
+	public class AlarmEventArgs {
+		public int alarmTime { get; set; }
+	}
+	public delegate void AlarmHandler(object sender, AlarmEventArgs args);
+
+	public class Clock {
+		public event TikTokHandler Tiking;
+		public event AlarmHandler Alarming;
+		
+		private int time;
+		public int alarmTime { get; set; }
+		private Thread thread;
+
+		public Clock() {
+			time = 0;
+			thread = new Thread(Run);
+			thread.Start();
+		}
+
+		private void Run() {
+			while (true) {
+				Thread.Sleep(1000);
+				time++;
+				TikTokEventArgs tiktokArgs = new TikTokEventArgs() {
+					time = this.time
+				};
+				Tiking(this, tiktokArgs);
+				if (time == alarmTime) {
+					AlarmEventArgs alarmArgs = new AlarmEventArgs() {
+						alarmTime = this.alarmTime
+					};
+					Alarming(this, alarmArgs);
+				}
+			}
+		}
+	}
+
+	public class Program {
+		static public void ListenTiktok(object sender, TikTokEventArgs args) {
+			Console.WriteLine($"Tiktok at {args.time}");
+		}
+		static public void ListenAlarm(object sender, AlarmEventArgs args) {
+			Console.WriteLine($"Alarming!!!!!! now is {args.alarmTime}");
+		}
+		static public void Main(string[] s) {
+			Clock clock = new Clock();
+			clock.alarmTime = 10;
+			clock.Tiking += ListenTiktok;
+			clock.Alarming += ListenAlarm;
+			while (true) {
+				
+			}
+			return;
+		}
+	}
+}
